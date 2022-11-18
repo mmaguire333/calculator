2375e19bdaaa17dd8cfbe0c611e1f0ae56a1fbb1
Matt <mmaguire333@gmail.com>
2022-11-18 14:46:29 -0700

    Get calculator buttons working and outputting correct results

--- script.js
+++ script.js
@@ -17,3 +17,93 @@ function divide(a, b) {
 
     return a / b;
 }
+
+function operate(a, b, operator) {
+    if(operator === "+") {
+        return add(a, b);
+    }
+
+    if(operator === "-") {
+        return subtract(a, b);
+    }
+
+    if(operator === "*") {
+        return multiply(a, b);
+    }
+
+    if(operator === "/") {
+        return divide(a, b);
+    }
+}
+
+const screen = document.querySelector('.screen');
+const numberButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
+const equalsButton = document.querySelector('.equals');
+const clearButton = document.querySelector('.clear');
+
+let previousNumber = "";
+let currentNumber = "";
+let currentOperation = "";
+let lastClicked = "";
+let expression = "";
+
+equalsButton.addEventListener('click', () => {
+    if(previousNumber === "" || currentNumber === "" || currentOperation === "") {
+        return;
+    }
+    let result = operate(parseInt(previousNumber), parseInt(currentNumber), currentOperation);
+    screen.textContent = result;
+    previousNumber = result;
+    currentNumber = "";
+    lastClicked = "operation";
+});
+
+clearButton.addEventListener('click', () => {
+    previousNumber = "";
+    currentNumber = "";
+    currentOperation = "";
+    lastClicked = "";
+    screen.textContent = "";
+});
+
+for(let i = 0; i < numberButtons.length; i++) {
+    numberButtons[i].addEventListener('click', () => {
+        if(currentOperation === "") {
+            previousNumber += numberButtons[i].textContent;
+        } else {
+            if(lastClicked === "operation") {
+                screen.textContent = "";
+            }
+            currentNumber += numberButtons[i].textContent;
+        }
+        appendNumber(numberButtons[i].textContent);
+        lastClicked = "number";
+    });
+}
+
+for(let i = 0; i < operatorButtons.length; i++) {
+    operatorButtons[i].addEventListener('click', () => {
+        if(currentOperation === "") {
+            currentOperation = operatorButtons[i].textContent;
+            lastClicked = "operation";
+        } else {
+            if(lastClicked === "operation") {
+                currentOperation = operatorButtons[i].textContent;
+                return;
+            }
+            let result = operate(parseInt(previousNumber), parseInt(currentNumber), currentOperation);
+            screen.textContent = result;
+            previousNumber = result;
+            currentNumber = "";
+            currentOperation = operatorButtons[i].textContent;
+            lastClicked = "operation";
+        }
+    });
+}
+
+function appendNumber(value) {
+    screen.textContent += value;
+}
+
+
